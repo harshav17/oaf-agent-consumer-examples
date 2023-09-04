4ab33b8613c441db8ffa6512971b43dfa1f66274
harsha <harshav17@gmail.com>
2023-09-03 19:23:44 -0700

    wardrobe example

---
+++ src/wardrobe_recs/README.md
@@ -0,0 +1,8 @@
+## Create [openWeather](https://openweathermap.org) account
+From your profile, create and caputre API key
+
+
+## Setup .env file
+```
+OPEN_WEATHER_API_KEY=<>
+```
--- src/wardrobe_recs/index.ts
+++ src/wardrobe_recs/index.ts
@@ -1,5 +1,14 @@
-import { ChatCompletionFunctions } from "openai";
+import * as dotenv from "dotenv";
+dotenv.config();
 
+import dedent from "dedent";
+import { OafOptions, callOaf } from "oaf-agent";
+import { ChatCompletionFunctions, ChatCompletionRequestMessage, ChatCompletionRequestMessageRoleEnum, Configuration } from "openai";
+import { Writable } from "stream";
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
 
 type weatherProps = {
     lat: number;
@@ -38,8 +47,46 @@ const functionsForModel: ChatCompletionFunctions[] = [
     },
 ]
 
+const finString = "[finished]";
+
+const zero_cot = dedent`
+    You are wardrobe assistant. Given the city of the user, you must recommend the user what to wear.
+
+    Respond to all future prompts with ${finString}, once you think you are finished with the story.
+`;
+
 async function main() {
-    console.log("TODO");
+    let messages: ChatCompletionRequestMessage[] = [
+        {
+            role: ChatCompletionRequestMessageRoleEnum.System,
+            content: zero_cot,
+        },
+        {
+            role: ChatCompletionRequestMessageRoleEnum.User,
+            content: "San Francisco",
+        }
+    ];
+    const stream = new Writable({
+        write(chunk, encoding, callback) {
+            // print without newline
+            process.stdout.write(chunk.toString());
+            callback();
+        },
+    });
+    stream.on('error', (err) => {
+        console.error(err);
+    });
+
+    const funs: Record<string, (...args: any[]) => any> = {
+        getCurrentWeather: getCurrentWeather
+    };
+    
+    const oafOptions: OafOptions = {
+        finString,
+        funcs: funs,
+        funcDescs: functionsForModel,
+    }
+    await callOaf(messages, stream, configuration, oafOptions);
 }
 
 main();
